438ac4128f21bb4fd00bacae6eb319e893353138
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-22 17:10:55 +0200

    email done

--- email/js/script.js
+++ email/js/script.js
@@ -7,18 +7,20 @@ const risposta = document.getElementById("risposta")
 searchButton.addEventListener('click', function(){
     
     let flag = false
-    let emailUser = document.getElementById("user-email").value
+    let emailUser = document.getElementById("user-email")
 
     for ( let i = 0; i < emails.length; i++) {
-        if (emails[i].toLowerCase() === emailUser.toLowerCase()){            
+        if (emails[i].toLowerCase() === emailUser.value.toLowerCase()){            
             flag = true
         }
     }
 
     if  ( flag === true){
-        risposta.innerHTML = `${emailUser.toUpperCase()} HA ACCESSO AL SITO`
+        risposta.innerHTML = `${emailUser.value.toUpperCase()} HA ACCESSO AL SITO`
     } else{
-        risposta.innerHTML = `${emailUser.toUpperCase()} NON HA ACCESSO AL SITO`
+        risposta.innerHTML = `${emailUser.value.toUpperCase()} NON HA ACCESSO AL SITO`
     }
+
+    emailUser.value = ''
 }
 )
